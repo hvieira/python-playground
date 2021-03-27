4ce23bdb8ffcfd426051df456ac9e9a27ac8413b
hvieira <scythe999@gmail.com>
2021-03-27 11:12:28 +0000

    Implement the collection mixin for the binary tree

--- data-structures/src/mypystructs/binarytree.py
+++ data-structures/src/mypystructs/binarytree.py
@@ -1,9 +1,49 @@
-class BinaryTree:
+from itertools import chain
+
+from collections.abc import Collection
+
+
+class BinaryTree(Collection):
 
     def __init__(self, value, left, right):
         self._value = value
         self._left = left
         self._right = right
+
+    def __len__(self):
+        size = 1
+        if self._left is not None:
+            size += len(self._left)
+
+        if self._right is not None:
+            size += len(self._right)
+
+        return size
+
+    def __iter__(self):
+        if self._left is None:
+            left_iter = iter([])
+        else:
+            left_iter = iter(self._left)
+
+        if self._right is None:
+            right_iter = iter([])
+        else:
+            right_iter = iter(self._right)
+
+        return chain(left_iter, [self._value], right_iter)
+
+    # TODO this is a left depth first search and is not log(n) complexity as one would expect for a balanced binary
+    #  tree. Add functionality to create a balanced tree and then fix this
+    def __contains__(self, item):
+        if item == self._value:
+            return True
+        elif self._left is not None and item in self._left:
+            return True
+        elif self._right is not None and item in self._right:
+            return True
+        else:
+            return False
 
     def sum(self):
         total = self._value
--- data-structures/src/tests/test_binary_tree.py
+++ data-structures/src/tests/test_binary_tree.py
@@ -1,7 +1,78 @@
 from mypystructs.binarytree import BinaryTree
 
+import pytest
+
+
+def balanced_tree():
+    return BinaryTree(
+        value=5,
+        left=BinaryTree(2,
+                        left=BinaryTree(1, None, None),
+                        right=BinaryTree(3,
+                                         left=None,
+                                         right=BinaryTree(4, None, None)),
+                        ),
+        right=BinaryTree(7,
+                         left=BinaryTree(6, None, None),
+                         right=BinaryTree(8, None, None),
+                         )
+    )
+
 
 class TestBinaryTree:
+
+    @pytest.mark.parametrize(
+        "tree, element_in_not_tree",
+        list(map(lambda num: (balanced_tree(), num), [-1, -2, 0, 10, 1000, -1000]))
+    )
+    def test_contains_false(self, tree, element_in_not_tree):
+        assert element_in_not_tree not in tree
+
+    @pytest.mark.parametrize(
+        "tree, element_in_tree",
+        list(map(lambda num: (balanced_tree(), num), range(1, 8+1)))
+    )
+    def test_contains_true(self, tree, element_in_tree):
+        assert element_in_tree in tree
+
+    @pytest.mark.parametrize(
+        "tree, expected_size",
+        [
+            (BinaryTree(0, None, None), 1),
+            (BinaryTree(
+                value=0,
+                left=BinaryTree(1, None, None),
+                right=BinaryTree(3, None, None)
+            ), 3),
+            (BinaryTree(
+                value=5,
+                left=BinaryTree(2,
+                                left=BinaryTree(1, None, None),
+                                right=BinaryTree(3, None, None),
+                                ),
+                right=BinaryTree(10, None, None)
+            ), 5),
+            (BinaryTree(
+                value=1,
+                left=BinaryTree(0, None, None),
+                right=BinaryTree(3,
+                                 left=BinaryTree(2, None, None),
+                                 right=BinaryTree(4, None, None),
+                                 )
+            ), 5),
+            (balanced_tree(), 8)
+        ]
+    )
+    def test_tree_size(self, tree, expected_size):
+        assert len(tree) == expected_size
+
+    def test_iterator(self):
+        expected_order = list(range(1, 8+1))
+        gotten_elements = []
+        for element in balanced_tree():
+            gotten_elements.append(element)
+
+        assert gotten_elements == expected_order
 
     def test_sum_single_branch(self):
         assert BinaryTree(
