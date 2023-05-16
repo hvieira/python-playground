94a1e36f27d6b95195d05425fc0d6810567f7ccc
Hugo Vieira <hugo.r.f.s.vieira@gmail.com>
2023-05-16 14:21:56 +0100

    fix formatting

--- working-with-files/filesystem_api.py
+++ working-with-files/filesystem_api.py
@@ -1,5 +1,11 @@
+from pathlib import Path
+
+
 def get_file_line_by_line(filename):
     with open(filename, 'r', encoding="utf-8") as f:
         for line in f:
-                yield line
+            yield line
 
+
+def get_abs_path_for_file(filename):
+    return Path(filename).absolute()
