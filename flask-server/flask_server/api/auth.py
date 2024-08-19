3065f2128eb24c170ab12f666894dca5741b9548
hvieira <hugo.r.f.s.vieira@gmail.com>
2024-08-19 18:08:29 +0100

    add token auth tests

--- flask-server/flask_server/api/auth.py
+++ flask-server/flask_server/api/auth.py
@@ -1,6 +1,6 @@
 
 from flask import (
-    Blueprint, jsonify, request
+    Blueprint, abort, jsonify, request
 )
 from sqlalchemy import select
 
@@ -9,6 +9,7 @@ from flask_server.models.user import User, UserToken
 from flask_server.api.requests import AuthTokenRequest
 from flask_server.api.responses import UserTokenResponse
 from flask_server.api.errors import InvalidRequest
+from werkzeug.security import check_password_hash
 
 
 bp = Blueprint('auth_api', __name__, url_prefix='/oauth')
@@ -24,6 +25,11 @@ def generate_token():
         # TODO add a user "service" with these sort of functions for DRY purposes
         user = dbAlchemy.session.execute(select(User).where(User.username == token_request.username)).scalar()
         if user:
+
+            if not check_password_hash(user.password, token_request.password):
+                return jsonify({'message': 'invalid credentials'}), 401
+            
+
             token = UserToken.create(user_id=user.id)
             dbAlchemy.session.add(token)
             dbAlchemy.session.commit()
@@ -31,5 +37,4 @@ def generate_token():
             return jsonify(UserTokenResponse.from_token(token).to_dict())
 
         else:
-            # TODO implement
-            pass
+            return jsonify({'message': 'invalid credentials'}), 401
--- flask-server/tests/api/test_auth.py
+++ flask-server/tests/api/test_auth.py
@@ -3,9 +3,11 @@ from unittest.mock import MagicMock, patch
 from flask import Flask
 from flask.testing import FlaskClient
 from freezegun import freeze_time
+import pytest
 from flask_server.models.user import User, UserToken
 from flask_server.db import dbAlchemy
 from sqlalchemy import select
+from werkzeug.security import generate_password_hash
 
 
 @freeze_time('2024-08-13 12:17:11', tz_offset=0)
@@ -13,7 +15,8 @@ from sqlalchemy import select
 def test_get_oauth_token_password_grant(rnd_choices_mock: MagicMock,  
                                         client: FlaskClient, 
                                         app: Flask):
-    user = User(username='hugo', password='test')
+    plain_passwd = 'testzzz'
+    user = User(username='hugo', password=generate_password_hash(plain_passwd))
     expected_token = 'blahblahblahblah'
     rnd_choices_mock.return_value = list(expected_token)
 
@@ -25,7 +28,7 @@ def test_get_oauth_token_password_grant(rnd_choices_mock: MagicMock,
         response = client.post('/oauth/token', json={
             'grant_type': 'password',
             'username': user.username,
-            'password': user.password,
+            'password': plain_passwd,
             'client_id': 'test',
             'client_secret': 'bananas'
         })
@@ -44,11 +47,82 @@ def test_get_oauth_token_password_grant(rnd_choices_mock: MagicMock,
         assert user1Tokens[0].expiry == datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=1)
 
 
+def test_get_oauth_token_password_non_existing_user(client: FlaskClient):
+    response = client.post('/oauth/token', json={
+        'grant_type': 'password',
+        'username': 'do not exist',
+        'password': 'do not exist passwd',
+        'client_id': 'test',
+        'client_secret': 'bananas'
+    })
+
+    assert response.status_code == 401
+
+
+def test_get_oauth_token_password_wrong_credentials(client: FlaskClient, app: Flask):
+    user = User(username='hugo', password='test')
+    with app.app_context():
+        dbAlchemy.session.add(user)
+        dbAlchemy.session.commit()
+        dbAlchemy.session.flush()
+    
+        response = client.post('/oauth/token', json={
+            'grant_type': 'password',
+            'username': user.username,
+            'password': 'wrong!',
+            'client_id': 'test',
+            'client_secret': 'bananas'
+        })
+
+        assert response.status_code == 401
+
+
+@pytest.mark.parametrize(
+    'bad_token_request',
+    [
+        ({
+            'username': 'user',
+            'password': 'pass',
+            'client_id': 'test',
+            'client_secret': 'bananas'
+        }),
+        ({
+            'grant_type': 'password',
+            'password': 'pass',
+            'client_id': 'test',
+            'client_secret': 'bananas'
+        }),
+        ({
+            'grant_type': 'password',
+            'username': 'user',
+            'client_id': 'test',
+            'client_secret': 'bananas'
+        }),
+        ({
+            'grant_type': 'password',
+            'username': 'user',
+            'password': 'pass',
+            'client_secret': 'bananas'
+        }),
+        ({
+            'grant_type': 'password',
+            'username': 'user',
+            'password': 'pass',
+            'client_id': 'test',
+        }),
+        ({
+            'grant_type': 'password',
+            'username': 'user',
+            'password': 'pass',
+            'client_id': 'test',
+        }),
+    ]
+)
+def test_get_oauth_token_password_non_existing_user(client: FlaskClient, bad_token_request):
+    assert client.post('/oauth/token', json=bad_token_request).status_code == 400
+
+
 # TODO test client id
 # TODO test client secret
-# TODO test with user not existing
-# TODO test with wrong credentials
-# TODO test bad request
-
 # TODO when using token, check for expiry
 
