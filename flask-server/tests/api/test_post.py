dd0ae7a9cece7e6044a6b3cad6926330aec324ea
hvieira <hugo.r.f.s.vieira@gmail.com>
2024-08-22 11:35:37 +0100

    add test for no token present

--- flask-server/tests/api/test_post.py
+++ flask-server/tests/api/test_post.py
@@ -5,6 +5,14 @@ from flask.testing import FlaskClient
 from tests.conftest import AuthActions
 
 
+def test_api_get_posts_requires_bearer_token(client: FlaskClient, app: Flask, auth: AuthActions):
+    with app.app_context():
+        auth.register()
+
+        response = client.get('/api/posts')
+        
+        assert response.status_code == 401
+
 def test_api_get_posts_requires_non_expired_token(client: FlaskClient, app: Flask, auth: AuthActions):
     with app.app_context():
         auth.register()
@@ -33,7 +41,6 @@ def test_api_get_posts_no_posts(client: FlaskClient, app: Flask, auth: AuthActio
 
 # TODO test pagination
 # TODO test invalid token
-# TODO test no token
 # TODO test create post
 # TODO test update post
 # TODO test delete post
