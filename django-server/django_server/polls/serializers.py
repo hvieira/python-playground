f3e8890888e4550f0a209b782683d6171d06ef3c
hvieira <hugo.r.f.s.vieira@gmail.com>
2024-08-27 16:46:12 +0100

    renamed choice_set field to choices which makes more sense

--- django-server/django_server/polls/serializers.py
+++ django-server/django_server/polls/serializers.py
@@ -2,15 +2,17 @@ from rest_framework import serializers
 from .models import Choice, Question
 
 
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        fields = ['id', "question_text", "pub_date", 'choice_set']
-        depth = 1
-        
-
-
 class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
         fields = ['id', 'choice_text', 'votes']
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['id', "question_text", "pub_date", 'choices']
+        depth = 1
+
+    choices = ChoiceSerializer(required=False, many=True, source='choice_set')
+
+
