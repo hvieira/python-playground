1d8167c0bd6b4c54a528256e8c227b7a34965715
hvieira <hugo.r.f.s.vieira@gmail.com>
2024-08-27 16:34:56 +0100

    add choice json to question API response

--- django-server/django_server/polls/serializers.py
+++ django-server/django_server/polls/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import Question
+from .models import Choice, Question
 
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
-        fields = ['id', "question_text", "pub_date"]
+        fields = ['id', "question_text", "pub_date", 'choice_set']
+        depth = 1
+        
+
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ['id', 'choice_text', 'votes']
