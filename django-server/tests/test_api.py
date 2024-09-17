4289bfc1b1dec7fc92f08cc61acee83709d4b948
hvieira <hugo.r.f.s.vieira@gmail.com>
2024-09-17 09:40:37 +0100

    add test to create poll

--- django-server/tests/test_api.py
+++ django-server/tests/test_api.py
@@ -7,55 +7,68 @@ from polls.models import Question
 
 
 @pytest.mark.django_db
-def test_get_polls_nothing_in_db(api_client: APIClient):
-    response = api_client.get('/api/polls/')
-    assert response.data == []
+class TestPollsApi():
 
+    def test_get_polls_nothing_in_db(self, api_client: APIClient):
+        response = api_client.get('/api/polls/')
+        assert response.data == []
 
-@pytest.mark.django_db
-def test_get_polls_with_existing_polls(api_client: APIClient):
-    q1 = Question(question_text='Thoughts about Django?', pub_date=timezone.now())
-    q2 = Question(question_text='What about X?', pub_date=timezone.now())
+    def test_get_polls_with_existing_polls(self, api_client: APIClient):
+        q1 = Question(question_text='Thoughts about Django?', pub_date=timezone.now())
+        q2 = Question(question_text='What about X?', pub_date=timezone.now())
 
-    q1.save()
-    q2.save()
+        q1.save()
+        q2.save()
 
-    q2.choice_set.create(choice_text='Really bad')
-    q2.choice_set.create(choice_text='Meh')
-    q2.choice_set.create(choice_text='Good enough')
+        q2.choice_set.create(choice_text='Really bad')
+        q2.choice_set.create(choice_text='Meh')
+        q2.choice_set.create(choice_text='Good enough')
 
-    response = api_client.get('/api/polls/')
-    
-    # TODO datetime objects do not support Z (Zulu time suffix) or any other military suffixes
-    # one solution would be to use explicit time zone - e.g. "+00:00", in this case. This would need a change in the serializers
-    assert response.data == [
-        {
-            'id': 1,
-            'question_text': q1.question_text,
-            'pub_date': q1.pub_date.isoformat().replace('+00:00', 'Z'),
-            'choices': []
+        response = api_client.get('/api/polls/')
 
-        },
-        {
-            'id': 2,
-            'question_text': q2.question_text,
-            'pub_date': q2.pub_date.isoformat().replace('+00:00', 'Z'),
-            'choices': [
-                {
-                    'id': 1, 
-                    'choice_text': 'Really bad',
-                    'votes': 0
-                },                
-                {
-                    'id': 2, 
-                    'choice_text': 'Meh',
-                    'votes': 0
-                },                
-                {
-                    'id': 3, 
-                    'choice_text': 'Good enough',
-                    'votes': 0
-                },
-            ]
-        }
-    ]
+        # TODO datetime objects do not support Z (Zulu time suffix) or any other military suffixes
+        # one solution would be to use explicit time zone - e.g. "+00:00", in this case. This would need a change in the serializers
+        assert response.data == [
+            {
+                'id': 1,
+                'question_text': q1.question_text,
+                'pub_date': q1.pub_date.isoformat().replace('+00:00', 'Z'),
+                'choices': []
+
+            },
+            {
+                'id': 2,
+                'question_text': q2.question_text,
+                'pub_date': q2.pub_date.isoformat().replace('+00:00', 'Z'),
+                'choices': [
+                    {
+                        'id': 1, 
+                        'choice_text': 'Really bad',
+                        'votes': 0
+                    },                
+                    {
+                        'id': 2, 
+                        'choice_text': 'Meh',
+                        'votes': 0
+                    },                
+                    {
+                        'id': 3, 
+                        'choice_text': 'Good enough',
+                        'votes': 0
+                    },
+                ]
+            }
+        ]
+
+    def test_create_poll(self, api_client: APIClient):
+        question_text = 'What is the best framework?'
+        response = api_client.post(
+            '/api/polls/', 
+            data={
+                'question_text': question_text
+            }
+        )
+
+        assert response.data['id']
+        assert response.data['question_text'] == question_text
+        assert response.data['choices'] == []
