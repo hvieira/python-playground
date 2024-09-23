21214d6de675544a79fef8f6671c23f1a51e0ff4
hvieira <hugo.r.f.s.vieira@gmail.com>
2024-09-23 10:57:53 +0100

    remove unnecessary code for requests - serializers are enough

--- django-api/store_api/views.py
+++ django-api/store_api/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
 from store_api.models import User
-from store_api.requests import CreateUserRequest
 from store_api.serializers import CreateUserRequestSerializer
 
 
@@ -14,10 +13,6 @@ class UserViewSet(ViewSet):
     def create(self, request: Request):
         serializer = CreateUserRequestSerializer(data=request.data)
         serializer.is_valid()
-
-        # TODO there's no need for this request class it seems
-        # request = CreateUserRequest(**serializer.validated_data)
-
 
         create_user = User.objects.create_user(**serializer.validated_data)
 
