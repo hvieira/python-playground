4ef7e676d0edb7767576e8af115825fb12f748e9
hvieira <hugo.r.f.s.vieira@gmail.com>
2024-10-16 16:58:21 +0100

    simplify assertions

--- django-api/tests/integration/api/test_products.py
+++ django-api/tests/integration/api/test_products.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 import pytest
 from django.test import Client
 from oauth2_provider.models import Application
@@ -56,9 +58,12 @@ class TestUserApi:
         }
 
         product_in_db = Product.objects.get(id=assigned_product_id)
-        assert product_in_db.title == product_title
-        assert product_in_db.description == product_description
-        assert product_in_db.price == product_price
+        assert product_in_db == Product(
+            id=UUID(assigned_product_id),
+            title=product_title,
+            description=product_description,
+            price=product_price,
+        )
         assert list(product_in_db.stock.all()) == [
             ProductStock(
                 id=1,
