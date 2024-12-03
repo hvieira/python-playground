8cec8302e8ca3937b2018ddffc294c22ea49af7e
hvieira <hugo.r.f.s.vieira@gmail.com>
2024-12-03 16:57:31 +0000

    ensure non staff users cannot create tags

--- django-api/tests/integration/api/test_tag_management.py
+++ django-api/tests/integration/api/test_tag_management.py
@@ -4,8 +4,13 @@ import pytest
 from django.test import Client
 from oauth2_provider.models import Application
 
-from store_api.models import User
+from store_api.models import Tag, User
 from tests.conftest import AuthActions
+
+CREATE_TAG_PAYLOAD = {
+    "name": "beautiful",
+    "description": "Tagged entity is deemed beautiful",
+}
 
 
 @pytest.mark.django_db()
@@ -18,11 +23,6 @@ class TestTagAPI:
         default_staff_user: User,
         default_oauth_app: Application,
     ):
-        payload = {
-            "name": "beautiful",
-            "description": "Tagged entity is deemed beautiful",
-        }
-
         access_token = auth_actions.generate_api_access_token(
             default_staff_user, default_oauth_app
         )
@@ -30,7 +30,7 @@ class TestTagAPI:
         response = api_client.post(
             "http://testserver/api/tags/",
             content_type="application/json",
-            data=payload,
+            data=CREATE_TAG_PAYLOAD,
             headers={"Authorization": f"Bearer {access_token.token}"},
         )
 
@@ -42,9 +42,35 @@ class TestTagAPI:
         assigned_id = response.json()["id"]
         assert response.json() == {
             "id": assigned_id,
-            "name": "beautiful",
-            "description": "Tagged entity is deemed beautiful",
+            "name": CREATE_TAG_PAYLOAD["name"],
+            "description": CREATE_TAG_PAYLOAD["description"],
         }
+
+        assert Tag.objects.get(id=assigned_id) == Tag(
+            id=UUID(assigned_id),
+            name=CREATE_TAG_PAYLOAD["name"],
+            description=CREATE_TAG_PAYLOAD["description"],
+        )
+
+    def test_non_staff_users_cannot_create_tags(
+        self,
+        api_client: Client,
+        auth_actions: AuthActions,
+        default_user: User,
+        default_oauth_app: Application,
+    ):
+        access_token = auth_actions.generate_api_access_token(
+            default_user, default_oauth_app
+        )
+
+        response = api_client.post(
+            "http://testserver/api/tags/",
+            content_type="application/json",
+            data=CREATE_TAG_PAYLOAD,
+            headers={"Authorization": f"Bearer {access_token.token}"},
+        )
+
+        assert response.status_code == 403
 
     # TODO staff user can list tags
 
