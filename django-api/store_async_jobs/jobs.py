314e770f06ca4a8ed082b5dd05f2583b730055a6
hvieira <hugo.r.f.s.vieira@gmail.com>
2025-02-17 11:26:33 +0000

    configure background jobs module to be executable

--- django-api/store_async_jobs/jobs.py
+++ django-api/store_async_jobs/jobs.py
@@ -1,12 +1,18 @@
+import logging
+import os
 from datetime import datetime, timedelta, timezone
 
 from apscheduler.schedulers.background import BlockingScheduler
 from django.db import transaction
 
-from store_api.models import Order
+LOGGER = logging.getLogger(__name__)
 
 
 def cancel_elapsed_unconfirmed_orders(confirmation_max_duration_seconds: int):
+    # this allows django to be configured first, before loading the models module which requires such config
+    from store_api.models import Order
+
+    LOGGER.debug("Querying for orders to be cancelled...")
     now_utc = datetime.now(timezone.utc)
     elasped_time = now_utc - timedelta(seconds=confirmation_max_duration_seconds)
     orders_to_cancel = (
@@ -19,9 +25,9 @@ def cancel_elapsed_unconfirmed_orders(confirmation_max_duration_seconds: int):
         for order in orders_to_cancel:
             order.cancel()
             order.save()
+            LOGGER.info(f"cancelled order {order.id}")
 
 
-# TODO to be started by a separate process
 def start(confirmation_max_duration_seconds: int, interval_seconds: int):
     scheduler = BlockingScheduler()
     scheduler.add_job(
@@ -31,6 +37,23 @@ def start(confirmation_max_duration_seconds: int, interval_seconds: int):
         seconds=interval_seconds,
         coalesce=True,
         max_instances=1,
+        next_run_time=datetime.now(timezone.utc),
     )
 
     scheduler.start()
+
+
+if __name__ == "__main__":
+
+    confirmation_max_duration_seconds = os.getenv(
+        "JOB_ORDER_CANCEL_PENDING_ORDERS_TIME_SECONDS", 30
+    )
+    interval_seconds = os.getenv("JOB_ORDER_CANCEL_PENDING_ORDERS", 5)
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_api.settings")
+
+    import django
+
+    django.setup()
+
+    start(confirmation_max_duration_seconds, interval_seconds)
