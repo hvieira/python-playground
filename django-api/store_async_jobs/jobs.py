5d57612df596b64655642424a3bd1384a8e63209
hvieira <hugo.r.f.s.vieira@gmail.com>
2025-02-20 17:50:11 +0000

    add log level option to background jobs

--- django-api/store_async_jobs/jobs.py
+++ django-api/store_async_jobs/jobs.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import os
 from datetime import datetime, timedelta, timezone
@@ -5,21 +6,16 @@ from datetime import datetime, timedelta, timezone
 from apscheduler.schedulers.background import BlockingScheduler
 from django.db import transaction
 
-LOGGER = logging.getLogger(__name__)
-log_level = os.environ.get("LOG_LEVEL", "INFO").upper()
-LOGGER.setLevel(log_level)
-
 
 def cancel_elapsed_unconfirmed_orders(confirmation_max_duration_seconds: int):
     # this allows django to be configured first, before loading the models module which requires such config
     from store_api.models import Order
 
-    LOGGER.debug("Querying for orders to be cancelled...")
+    logging.debug("Querying for orders to be cancelled...")
     now_utc = datetime.now(timezone.utc)
     to_confirm_threshold_time = now_utc - timedelta(
         seconds=confirmation_max_duration_seconds + 1
     )
-    print(f"threshold is {to_confirm_threshold_time}")
     orders_to_cancel = (
         Order.objects.filter(state=Order.States.PENDING)
         .filter(created__lte=to_confirm_threshold_time)
@@ -30,7 +26,7 @@ def cancel_elapsed_unconfirmed_orders(confirmation_max_duration_seconds: int):
         for order in orders_to_cancel:
             order.cancel()
             order.save()
-            LOGGER.info(f"cancelled order {order.id}")
+            logging.info(f"cancelled order {order.id}")
 
 
 def start(confirmation_max_duration_seconds: int, interval_seconds: int):
@@ -45,17 +41,37 @@ def start(confirmation_max_duration_seconds: int, interval_seconds: int):
         next_run_time=datetime.now(timezone.utc),
     )
 
+    logging.debug("starting scheduler...")
+    print("starting!!!")
     scheduler.start()
 
 
 if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(
+        prog="store background jobs",
+        description="Application that runs store background jobs",
+    )
+    argparser.add_argument(
+        "-l",
+        "--log-level",
+        choices=[
+            logging.getLevelName(logging.ERROR),
+            logging.getLevelName(logging.WARN),
+            logging.getLevelName(logging.INFO),
+            logging.getLevelName(logging.DEBUG),
+        ],
+        default=logging.INFO,
+    )
+
+    args = argparser.parse_args()
+    logging.root.setLevel(args.log_level)
+
     confirmation_max_duration_seconds = os.getenv(
         "JOB_ORDER_CANCEL_PENDING_ORDERS_TIME_SECONDS", 30
     )
     interval_seconds = os.getenv("JOB_ORDER_CANCEL_PENDING_ORDERS_INTERVAL_SECONDS", 5)
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_api.settings")
-
     import django
 
     django.setup()
